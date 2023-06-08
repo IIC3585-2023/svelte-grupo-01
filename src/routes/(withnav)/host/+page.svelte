65248c353084a71e5e17bb07dfed748bcb826b84
Benjamín Vicente <benjavicente@uc.cl>
2023-06-07 21:32:22 -0400

    initial commit

---
+++ src/routes/(withnav)/host/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import EditGameForm from './EditGameForm.svelte';
+	import { gamesDB, currentGame } from './gamesDB';
+
+	let newGameName: string = '';
+</script>
+
+<h1>Host a Game</h1>
+
+{#if $gamesDB.status === 'loading'}
+	<div>Cargando</div>
+{:else if $currentGame}
+	<EditGameForm game={$currentGame} on:submit={(e) => gamesDB.updateGame(e.detail)} />
+{:else}
+	<form on:submit={() => gamesDB.createGame({ name: newGameName })}>
+		<input type="text" bind:value={newGameName} />
+		<button type="submit">Create</button>
+	</form>
+	<ul>
+		{#each $gamesDB.games as game}
+			<li>
+				{game.name}
+				<a href={`/host?game=${game.id}`}>Edit</a>
+				<button on:click={() => gamesDB.deleteGame({ id: game.id })}>Delete</button>
+				<a href={`/host/start?game=${game.id}`}>Start</a>
+			</li>
+		{/each}
+	</ul>
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<main>
+	<h1>Competitive Wordle</h1>
+	<nav>
+		<a href="/host">Host</a>
+		<a href="/play">Play</a>
+	</nav>
+</main>
+
+<style>
+	h1 {
+		font-size: theme('fontSize.4xl');
+		font-weight: theme('fontWeight.bold');
+		color: theme('textColor.orange.950');
+		margin-bottom: theme('margin.4');
+	}
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+	nav {
+		display: flex;
+		gap: theme('gap.4');
+		justify-items: center;
+	}
+	a {
+		padding: theme('padding.2') theme('padding.4');
+		background-color: theme('colors.orange.400');
+		color: theme('colors.orange.50');
+		width: theme('width.24');
+		text-align: center;
+	}
+</style>
