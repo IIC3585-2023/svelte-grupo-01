97db12bc44fa3a5cf5a6391bd27b233f2edfe55b
Benjamín Vicente <benjavicente@uc.cl>
2023-06-07 22:41:45 -0400

    add slash

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.BASE_PATH || ''
+			base: process.env.BASE_PATH ? `/${process.env.BASE_PATH}` : undefined
 		}
 	}
 };
